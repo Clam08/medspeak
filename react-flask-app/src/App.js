bf5408d8f8237534a5023df6968c33d9ac0752ba
Waseem Albarati <waseemalbarati@Waseems-MacBook-Air.local>
2020-07-12 12:26:12 -0700

    removed default app and started to add choose your language

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to medspeak
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=ub82Xb1C8os"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here
-        </a>
-      </header>
+      <h1>Choose your language:</h1> 
     </div>
   );
 }
