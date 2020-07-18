2215ad817cd37ca5a930816c96b7c3d289b6bb2c
Chloe <clam.e5r@gmail.com>
2020-07-18 16:59:18 -0400

    restructuring pics

--- myapp/app.js
+++ myapp/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', function (req, res){
-	res.sendfile(__dirname + '/pages/doctorpatient.html');
+	res.sendFile(__dirname + '/pages/doctorpatient.html');
 });
 
 app.listen(port, () => console.log('Example app listening at http://localhost:3000'))
