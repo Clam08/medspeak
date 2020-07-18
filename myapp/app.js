1cd971994a96661543f12584c8c643b715576752
Chloe <clam.e5r@gmail.com>
2020-07-18 17:00:21 -0400

    restructuring pics pt2

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.static('public'))
 
 app.get('/', function (req, res){
 	res.sendFile(__dirname + '/pages/doctorpatient.html');
