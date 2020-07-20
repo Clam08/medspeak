4ec59953c13862105f0cbd59b860c44f7ab37d5e
Chloe <clam.e5r@gmail.com>
2020-07-20 03:11:50 -0400

    worked on linking pages together (need to reorder some still oops)

--- myapp/app.js
+++ myapp/app.js
@@ -6,5 +6,14 @@ app.use(express.static('public'))
 app.get('/', function (req, res){
 	res.sendFile(__dirname + '/pages/home.html');
 });
+app.get('/patient-language', function (req, res){
+	res.sendFile(__dirname + '/pages/language1.html');
+});
+app.get('/doctor-language', function (req, res){
+	res.sendFile(__dirname + '/pages/language2.html');
+});
+app.get('/choose', function (req, res){
+	res.sendFile(__dirname + '/pages/doctorpatient.html');
+});
 
 app.listen(port, () => console.log('Example app listening at http://localhost:3000'))
