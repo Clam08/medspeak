5a1bc71fdb28d5f317189d845517f897a2ebb8a3
Chloe <clam.e5r@gmail.com>
2020-07-20 22:05:17 -0400

    fixed home button and added dummy patient portal page

--- myapp/app.js
+++ myapp/app.js
@@ -6,14 +6,20 @@ app.use(express.static('public'))
 app.get('/', function (req, res){
 	res.sendFile(__dirname + '/pages/home.html');
 });
-app.get('/patient-language', function (req, res){
+app.get('/user1-language', function (req, res){
 	res.sendFile(__dirname + '/pages/language1.html');
 });
-app.get('/doctor-language', function (req, res){
+app.get('/user2-language', function (req, res){
 	res.sendFile(__dirname + '/pages/language2.html');
 });
 app.get('/choose', function (req, res){
 	res.sendFile(__dirname + '/pages/doctorpatient.html');
+});
+app.get('/patient-portal', function (req, res){
+	res.sendFile(__dirname + '/pages/PatientPortal.html');
+});
+app.get('/doctor-portal', function (req, res){
+	res.sendFile(__dirname + '/pages/DoctorPortal.html');
 });
 
 app.listen(port, () => console.log('Example app listening at http://localhost:3000'))
